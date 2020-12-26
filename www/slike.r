acae42fedab16da49cf5cc2c3e491b25c71d112c
masterlord99 <jakob.jelencic1@gmail.com>
2020-12-26 12:01:21 +0000

    s

---
+++ www/slike.r
@@ -0,0 +1,93 @@
+library(shiny)
+
+
+icona_narobe <- fluidRow(
+  column(12,align="center",
+         icon("fas fa-exclamation-triangle", lib = "font-awesome")
+  ))
+
+icona_ok <- fluidRow(
+  column(12,align="center",
+         icon("fas fa-check-circle", lib = "font-awesome")
+  ))
+icona_ring <- fluidRow(
+  column(12, align="center",
+         
+         img(src="onering.jpg"),
+         br()
+  ))
+################################################################################ narejene slikice
+
+
+trio <-fluidRow(
+  column(12, align="center",
+         br(),
+         img(src="dwarf.jpg"),
+         br()))
+
+balin <-fluidRow(
+  column(12, align="right",
+         br(),
+         img(src="balin.jpg"),
+         br()))
+
+bilbo <-fluidRow(
+  column(12, align="center",
+         br(),
+         img(src="bilbo.jpg"),
+         br()))
+
+dwalin <-fluidRow(
+  column(12, align="center",
+         br(),
+         img(src="dwalin.jpg"),
+         br()))
+
+dwalin_hrbet <-fluidRow(
+  column(12, align="center",
+         br(),
+         img(src="dwalinhrbet.jpg"),
+         br()))
+
+fili <-fluidRow(
+  column(6, align="left",
+         br(),
+         img(src="fili.jpg"),
+         br()))
+
+gandalf <-fluidRow(
+  column(12, align="center",
+         br(),
+         img(src="gandalf.jpg"),
+         br()))
+
+gollum <-fluidRow(
+  column(12, align="center",
+         br(),
+         img(src="gollum.jpg"),
+         br()))
+
+kili <-fluidRow(
+  column(6, align="right",
+         br(),
+         img(src="kili.jpg"),
+         br()))
+
+legolas <-fluidRow(
+  column(12, align="right",
+         br(),
+         img(src="legolas.jpg"),
+         br()))
+
+thorin <-fluidRow(
+  column(12, align="center",
+         br(),
+         img(src="oakenshield.jpg"),
+         br()))
+
+smaug <-fluidRow(
+  column(12, align="center",
+         br(),
+         img(src="smaug.jpg"),
+         br()))
+
